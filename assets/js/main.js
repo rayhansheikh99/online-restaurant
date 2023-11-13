8680b651b1f2d1c3c49b048d0b0d1fb8d8255939
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2023-11-14 02:27:17 +0600

    home

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".custom-swiper-button-next",
+        prevEl: ".custom-swiper-button-prev",
+    },
+    breakpoints: {
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        540: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
